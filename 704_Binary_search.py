e04a89d6faa041a0760cff492d3c70947aa9b29d
NagiQing <1492309036@qq.com>
2024-08-14 16:15:07 +0800

    Create 704_Binary_search.py

---
+++ 704_Binary_search.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if nums == None or len(nums) == 0:
+            return -1
+        left, right = 0, len(nums) - 1
+        while(left <= right):
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+        return -1
