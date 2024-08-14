dc8c1a9d9450dc600d2076c9c5e03de441021dc4
QingPeng <1492309036@qq.com>
2024-08-15 00:10:22 +0800

    Update 989_Add_to_array-from_of_integer.py

--- 989_Add_to_array-from_of_integer.py
+++ 989_Add_to_array-from_of_integer.py
@@ -2,20 +2,13 @@ class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
         temp = 0
         num = deque(num)
+        total = k
         for i in range(len(num)-1, -1, -1):
-            remainder = k % 10
-            k //= 10
-            total = num[i] + remainder + temp
-            print(total, k)
-            temp = total // 10
+            total = num[i] + total
             num[i] = total % 10
-        
-        while k != 0 or temp == 1:
-            remainder = k % 10
-            k //= 10
-            total = remainder + temp
-            print(total, k)
-            temp = total // 10
+            total //= 10
+        while total > 0:
             num.appendleft(total % 10)
+            total //= 10
 
         return list(num)
