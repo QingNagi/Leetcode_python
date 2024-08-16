8269a73cab19ca648bf7ef27e5d815727875909e
NagiQing <1492309036@qq.com>
2024-08-16 18:36:03 +0800

    100

---
+++ Hot_100/189_Rotate_array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        def reverse(i: int, j: int) -> None:
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+
+        n = len(nums)
+        k %= n  # 轮转 k 次等于轮转 k%n 次
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
