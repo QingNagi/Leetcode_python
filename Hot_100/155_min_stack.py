df02ac755b93f978fa3a198e7c7a3cce2c364f07
NagiQing <1492309036@qq.com>
2024-08-22 11:41:32 +0800

    Create 155_min_stack.py

---
+++ Hot_100/155_min_stack.py
@@ -0,0 +1,15 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]: 
+            self.min_stack.append(val)
+    def pop(self) -> None:
+        if self.stack.pop() == self.min_stack[-1]:
+            self.min_stack.pop()
+    def top(self) -> int:
+        return self.stack[-1]
+    def getMin(self) -> int:
+        return self.min_stack[-1]
