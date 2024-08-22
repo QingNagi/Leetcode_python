2b11c3fc3d55fb774dbcac8f0a81b98e427e9798
NagiQing <1492309036@qq.com>
2024-08-22 16:13:47 +0800

    Create 739_Daily_temperatures.py

---
+++ Hot_100/739_Daily_temperatures.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        ans = [0] * n
+        st = []
+        for i in range(n - 1, -1, -1):
+            t = temperatures[i]
+            while st and t >= temperatures[st[-1]]:
+                st.pop()
+            if st:
+                ans[i] = st[-1] - i
+            st.append(i)
+        return ans
