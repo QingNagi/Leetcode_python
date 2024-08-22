e1f93044b175c72d4fd4618b7d36bcf62f9835f5
QingPeng <1492309036@qq.com>
2024-08-22 22:36:55 +0800

    Create 295_Find_median_from_data_stream.py

---
+++ Hot_100/295_Find_median_from_data_stream.py
@@ -0,0 +1,11 @@
+class MedianFinder:
+    def __init__(self):
+        self.A = [] 
+        self.B = [] 
+    def addNum(self, num: int) -> None:
+        if len(self.A) != len(self.B):
+            heappush(self.B, -heappushpop(self.A, num))
+        else:
+            heappush(self.A, -heappushpop(self.B, -num))
+    def findMedian(self) -> float:
+        return self.A[0] if len(self.A) != len(self.B) else (self.A[0] - self.B[0]) / 2.0
